53c45a1979f1c1662179da9e9e122936a933dadf
Jaga1321 <157679082+Jaga1321@users.noreply.github.com>
2024-01-25 09:14:10 +0100

    Add files via upload

---
+++ CSC201.java
@@ -0,0 +1,36 @@
+java
+System.out.print("Enter the first number: ");
+double num1 = scanner.nextDouble();
+
+System.out.print("Enter the second number: ");
+double num2 = scanner.nextDouble();
+
+System.out.print("Enter the operator (+, -, *, /): ");
+char operator = scanner.next().charAt(0);
+
+double result = 0;
+
+switch (operator) {
+    case '+':
+        result = num1 + num2;
+        break;
+    case '-':
+        result = num1 - num2;
+        break;
+    case '*':
+        result = num1 * num2;
+        break;
+    case '/':
+        if (num2 != 0) {
+            result = num1 / num2;
+        } else {
+            System.out.println("Error: Division by zero is undefined.");
+            return;
+        }
+        break;
+    default:
+        System.out.println("Error: Invalid operator.");
+        return;
+}
+
+System.out.println("Result: " + result);
